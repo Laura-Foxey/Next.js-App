b983ddcabbf34157c361f82879af3ad24d005dd7
Laura-Foxey <roura_sasunaru@hotmail.com>
2022-10-28 21:15:28 +0300

    add comments

--- pages/chucknorris/Quote.js
+++ pages/chucknorris/Quote.js
@@ -5,23 +5,25 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 
 
 const Quote = ({quote}) => {
-
     const [user, loading] = useAuthState(auth);
     const [vote, setVote] = useState(0);
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState('');
 
+    //fetch comments from db and stores in state
     const getDocuments = useMemo (() => {
         const q = query(collection(db, "comments"), where("id", "==", quote.id));
         getDocs(q).then((data) => data.forEach((d) => 
             setComments(prev =>[...prev, {comment: d.data().comment, user: d.data().user, createdAt: d.data().createdAt, id: quote.id, clicked: false}])))
     }, [quote.id]);
 
+    //fetch votes from db and store in state
     useEffect(() => {
             const docRef = doc(db, "votes", quote.id);
             getDoc(docRef).then((data) => setVote(data.data().votes));
         }, [vote])
 
+    //updates votes from db
     const updateVote = () => {
         setDoc(doc(db, "votes", quote.id), {
             id: quote.id,
@@ -29,6 +31,7 @@ const Quote = ({quote}) => {
           });
     }
 
+    //changed vote and updates to db
     const upvoting = () => {
         setVote(vote = vote + 1);
         updateVote();
@@ -42,16 +45,19 @@ const Quote = ({quote}) => {
         return;
     }
 
+    //comment form
     const submitComment = async (event) => {
         event.preventDefault();
         alert(`So your comment is ${event.target.comment.value}?`);
-        //add to db
+        //add comment to DB
         addDoc(collection(db, "comments"), {comment: event.target.comment.value, user: user.email, createdAt: Date.now(), id: quote.id})
         //temporarily stores it in local state until refresh when it fetches from DB
         setComments(prev =>[...prev, {comment: event.target.comment.value, user: user.email, createdAt: Date.now(), id: quote.id, clicked:false}])
         setNewComment('');
       };
 
+
+    //sets clicked to true on user click to expand comment details
     const itemDetails = (id) => {
         setComments(
           comments.map((com) =>
@@ -59,7 +65,8 @@ const Quote = ({quote}) => {
           )
         );
     };
-
+    
+    if (loading) return (<p> Please stand by. </p>)
     return (
         <>
             <h3> {quote.value} </h3>
@@ -83,7 +90,8 @@ const Quote = ({quote}) => {
             <div> 
                 <p>People who commented: {comments.length}</p> 
                 <ul>{comments.map((com) => <li key={com.createdAt+com.user} onClick={() => itemDetails(com.createdAt)}> 
-                    {com.user} at {new Date(com.createdAt).getDate()}.{new Date(com.createdAt).getMonth() + 1}.{new Date(com.createdAt).getFullYear()} {new Date(com.createdAt).getHours()}:{new Date(com.createdAt).getMinutes()}
+                    {com.user} at {new Date(com.createdAt).getDate()}.{new Date(com.createdAt).getMonth() + 1}.
+                    {new Date(com.createdAt).getFullYear()} {new Date(com.createdAt).getHours()}:{new Date(com.createdAt).getMinutes()}
                 {com.clicked && <div> {com.comment} </div>}
                 </li>)}
                 </ul>

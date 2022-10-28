9f8dc1f145b95a9e07340919fd1ac858769b26c3
Laura-Foxey <roura_sasunaru@hotmail.com>
2022-10-28 21:08:51 +0300

    fix comment view

--- pages/chucknorris/Quote.js
+++ pages/chucknorris/Quote.js
@@ -1,28 +1,26 @@
-import { db } from "../../firebaseConfig"
+import { db, auth } from "../../firebaseConfig"
 import { addDoc, doc, getDoc, getDocs, collection, query, setDoc, where} from "firebase/firestore";
-import { useEffect, useState } from "react";
-import {auth} from "./../../firebaseConfig";
+import { useEffect, useMemo, useState } from "react";
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 
 const Quote = ({quote}) => {
 
     const [user, loading] = useAuthState(auth);
-    const [vote, setVote] =useState(0);
+    const [vote, setVote] = useState(0);
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState('');
+
+    const getDocuments = useMemo (() => {
+        const q = query(collection(db, "comments"), where("id", "==", quote.id));
+        getDocs(q).then((data) => data.forEach((d) => 
+            setComments(prev =>[...prev, {comment: d.data().comment, user: d.data().user, createdAt: d.data().createdAt, id: quote.id, clicked: false}])))
+    }, [quote.id]);
 
     useEffect(() => {
             const docRef = doc(db, "votes", quote.id);
             getDoc(docRef).then((data) => setVote(data.data().votes));
-            const q = query(collection(db, "comments"), where("id", "==", quote.id));
-            getDocs(q).then((data) => data.forEach((d) => 
-                setComments(prev =>[...prev, {comment: d.data().comment, user: d.data().user, id: quote.id}])))
-        }, [quote.id])
-
-    useEffect(() => {} , [vote])
-
-    console.log(comments)
+        }, [vote])
 
     const updateVote = () => {
         setDoc(doc(db, "votes", quote.id), {
@@ -47,10 +45,20 @@ const Quote = ({quote}) => {
     const submitComment = async (event) => {
         event.preventDefault();
         alert(`So your comment is ${event.target.comment.value}?`);
-        setComments(prev =>[...prev, {comment: event.target.comment.value, user: user.email, id: quote.id}])
+        //add to db
+        addDoc(collection(db, "comments"), {comment: event.target.comment.value, user: user.email, createdAt: Date.now(), id: quote.id})
+        //temporarily stores it in local state until refresh when it fetches from DB
+        setComments(prev =>[...prev, {comment: event.target.comment.value, user: user.email, createdAt: Date.now(), id: quote.id, clicked:false}])
         setNewComment('');
-
       };
+
+    const itemDetails = (id) => {
+        setComments(
+          comments.map((com) =>
+            com.createdAt === id ? { ...com, clicked: !com.clicked } : com
+          )
+        );
+    };
 
     return (
         <>
@@ -73,9 +81,12 @@ const Quote = ({quote}) => {
             </button>
             </form>
             <div> 
-                <p>People who commented: </p> 
-                {/* <ul>{comments.map((com) => <li key={com.comment}>{com.user}</li>)}
-                </ul> */}
+                <p>People who commented: {comments.length}</p> 
+                <ul>{comments.map((com) => <li key={com.createdAt+com.user} onClick={() => itemDetails(com.createdAt)}> 
+                    {com.user} at {new Date(com.createdAt).getDate()}.{new Date(com.createdAt).getMonth() + 1}.{new Date(com.createdAt).getFullYear()} {new Date(com.createdAt).getHours()}:{new Date(com.createdAt).getMinutes()}
+                {com.clicked && <div> {com.comment} </div>}
+                </li>)}
+                </ul>
             </div>
         </>
     )
